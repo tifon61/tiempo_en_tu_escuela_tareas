65c2956d7be677005fe9f76aa43b0294e50555a9
tifon61 <mauriciobocanegra122@gmail.com>
2026-09-21 11:58:57 -0300

    Update QC Apps Script URL and token

--- apps-script-qc/qc-config.js
+++ apps-script-qc/qc-config.js
@@ -5,11 +5,11 @@
  * Calidad del Laboratorio de Análisis queda deshabilitada (el resto del
  * informe funciona igual).
  */
-const QC_APPS_SCRIPT_URL = "";
+const QC_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzwho5UwhIyZi3w6BelIMyTTkYVCyq3TQZIh6veYRzADrdWueQonQX0FMDyO8wWztTL/exec";
 
 /**
  * Tiene que coincidir con la Script Property TOKEN que configures en ESE
  * proyecto de Apps Script (es un backend separado del de observaciones,
  * puede tener su propio token distinto).
  */
-const QC_APPS_SCRIPT_TOKEN = "";
+const QC_APPS_SCRIPT_TOKEN = "Pagina1emaverde";
